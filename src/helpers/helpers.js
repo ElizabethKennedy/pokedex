4652b6e8130dad1244cc6860da705d64cd7ce7bf
Elizabeth Kennedy <110371718+ElizabethKennedy@users.noreply.github.com>
2022-12-01 19:39:00 -0800

    styling

---
+++ src/helpers/helpers.js
@@ -0,0 +1,26 @@
+export function filterPokemonByName(list, name) {
+    if (name) return list.filter((pokemon) =>
+    pokemon.name === name);
+    else
+    return list;
+}
+
+export function filterPokemonByType(list, type) {
+    if (type) return list.filter((pokemon) =>
+    pokemon.type.includes(type));
+    else
+    return list;
+}
+
+export function filterPokemonByWeaknesses(list, weaknesses) {
+    if (weaknesses) return list.filter((pokemon) =>
+    pokemon.weaknesses.includes(weaknesses));
+    else
+    return list;
+}
+
+export function getListOf(list, prop) {
+    return [...new Set(list.reduce((allItems, pokemon)=>
+        allItems.concat (pokemon[prop]),[])
+    )];
+}
