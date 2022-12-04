dc6be1fa0867e52bc41f7955c8870c7d303f9f45
Elizabeth Kennedy <110371718+ElizabethKennedy@users.noreply.github.com>
2022-12-03 21:02:57 -0800

    pokemon list added

---
+++ public/test/App.jest.spec.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import axiosMock from 'axios'
+import '@testing-library/jest-dom/extend-expect'
+import App from '../src/App'
+
+jest.mock('axios')
+
+describe('<App />', () => {
+  it('fetches data', async () => {
+    axiosMock.get.mockResolvedValueOnce(
+      {
+        data: {
+          results: [{ url: 'https://pokeapi.co/api/v2/pokemon/1/', name: 'bulbasaur', id: 1 }]
+        }
+      }
+    )
+    await (async () => {
+      render(<App />)
+    })
+    expect(axiosMock.get).toHaveBeenCalledTimes(1)
+    expect(axiosMock.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/?limit=50')
+  })
+
+  it('shows LoadingSpinner', async () => {
+    axiosMock.get.mockResolvedValueOnce({})
+    await (async () => {
+      const { getByAltText } = render(<App />)
+      // eslint-disable-next-line testing-library/prefer-screen-queries
+      expect(getByAltText('Loading...')).toBeVisible()
+    })
+  })
+
+  it('shows error', async () => {
+    axiosMock.get.mockRejectedValueOnce(new Error())
+    await (async () => {
+      render(<App />)
+    })
+    expect(screen.getByTestId('error')).toBeVisible()
+  })
+})
